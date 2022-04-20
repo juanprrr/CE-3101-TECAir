8feb1643143c8e8b8a844c372c63b65aaca841c6
Anthony <anthonymoro08@gmail.com>
2022-04-20 05:30:33 +0000

    Inmemory DB

--- TecAir.API/Program.cs
+++ TecAir.API/Program.cs
@@ -5,14 +5,15 @@ using TecAir.API.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddDbContext<TodoDbContext>(options => options.UseInMemoryDatabase("todo"));
-
+builder.Services.AddDbContext<ToDoDbContext>(options => options.UseInMemoryDatabase("todo"));
+/*
 var configuration = builder.Configuration;
 
 builder.Services.AddDbContext<ToDoDbContext>(options =>
 {
     options.UseNpgsql(configuration.GetConnectionString("Database"));
 });
+*/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
