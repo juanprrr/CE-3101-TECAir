4a9ae5d28f6b93c9fcaa0a91fd8bc61505d3a86e
Anthony <anthonymoro08@gmail.com>
2022-04-22 07:26:53 +0000

    [add] post request

--- TecAir.API/Program.cs
+++ TecAir.API/Program.cs
@@ -15,6 +15,17 @@ builder.Services.AddDbContext<ToDoDbContext>(options =>
 });
 */
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200/", "http://127.0.0.1:4200/")
+            .AllowAnyHeader()
+            .WithMethods("POST", "GET");
+        });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
