505ffb81d0748f9a2777ef7c63691327f230b0a6
juanprrr <54974335+juanprrr@users.noreply.github.com>
2022-04-22 23:34:58 -0600

    [UPDATE] #8 Post Routes to API

--- Frontend/TECAir-web/src/app/Components/flight-mngmt/flight-mngmt.component.ts
+++ Frontend/TECAir-web/src/app/Components/flight-mngmt/flight-mngmt.component.ts
@@ -1,5 +1,7 @@
+import { RouteService } from './../../Services/route.service';
+import { Route } from './../../Models/route';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-flight-mngmt',
   templateUrl: './flight-mngmt.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightMngmtComponent implements OnInit {
 
-  constructor() { }
+    newRoute: Route = new Route
+  constructor(private service:RouteService, private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  addRoute(newRoute:Route){
+    this.service.insertRoute(newRoute).subscribe(()=>
+    {
+      //this.router.navigate(['/'])
+    }, ()=>alert("No se pudo registrar ruta"))
+  }
 }
---
+++ Frontend/TECAir-web/src/app/Models/route.ts
@@ -0,0 +1,4 @@
+export class Route {
+    Id:number | undefined
+    Name:string | undefined
+}
